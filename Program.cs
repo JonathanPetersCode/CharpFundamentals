1fc1e91679adb082361ee094d0357d6b4222a7c7
Jonathan Peters <42306733+JonathanPetersCode@users.noreply.github.com>
2020-07-19 19:35:32 -0400

    add bitwise operation example

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+  class Program
+  {
+      static void Main(string[] args)
+      {
+          byte a = 16;
+          var l = a >> 5;
+          Console.WriteLine(l);
+      }
+    }
